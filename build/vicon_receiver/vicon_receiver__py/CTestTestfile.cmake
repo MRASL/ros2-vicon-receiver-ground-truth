2becf0be909afe0423ce1f1bf0306434225228e7
Hsiu-Tsu Shui <jjdavid1206@gmail.com>
2022-12-17 19:59:25 -0500

    vicon_receiver update

--- build/vicon_receiver/vicon_receiver__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hsiutsu/ros2_ws/src/ros2_vicon_receiver/build/vicon_receiver/vicon_receiver__py
-# Build directory: /home/hsiutsu/ros2_ws/src/ros2_vicon_receiver/build/vicon_receiver/vicon_receiver__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
